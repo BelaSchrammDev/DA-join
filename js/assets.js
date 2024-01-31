bb97bfc3c559a3f6f92c3b80c72ab67b884c2016
BelaSchrammDev <belaschramm@aol.de>
2024-01-31 16:49:38 +0100

    first commit

---
+++ js/assets.js
@@ -0,0 +1,17 @@
+const hexColors = [
+  '#0038FF',
+  '#00BEE8',
+  '#1FD7C1',
+  '#6E52FF',
+  '#9747FF',
+  '#C3FF2B',
+  '#FC71FF',
+  '#FF4646',
+  '#FF5EB3',
+  '#FF745E',
+  '#FF7A00',
+  '#FFA35E',
+  '#FFBB2B',
+  '#FFC701',
+  '#FFE62B',
+];
