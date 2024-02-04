a101740d17ea9da62c990fdddb57ea1ad3d5c65d
BelaSchrammDev <belaschramm@aol.de>
2024-02-04 22:53:13 +0100

    addtask: add change task category field and close dropdownlist when clicked outside the list

--- js/addtask.js
+++ js/addtask.js
@@ -1,4 +1,54 @@
 let openAssignedContactsList = false;
+let openTaskCategoryList = false;
+
+
+const taskCategorys = [
+    { id: 1, name: 'User Story' },
+    { id: 2, name: 'Techical Task' }
+]
+
+
+async function initAddtaskSite() {
+    await includeHTML();
+    renderAddtaskFields();
+}
+
+
+function renderAddtaskFields() {
+    const taskCategoryDiv = document.getElementById('edit_category_list');
+    let html = '';
+    for (let index = 0; index < taskCategorys.length; index++) {
+        const category = taskCategorys[index];
+        html += `<div onclick="selectTaskCategory(${index})"><span>${category.name}</span></div>`
+    }
+    taskCategoryDiv.innerHTML = html;
+}
+
+
+function openTaskCategoryDropDownList() {
+    closeAssignedContactsDropDownList();
+    if (openTaskCategoryList == true) return;
+    openTaskCategoryList = true;
+    const list = document.getElementById('edit_category_list');
+    document.getElementById('edit_category').style = 'border: 1px solid #29abe2;'
+    document.getElementById('edit_category').children[1].setAttribute('open', true);
+    list.style['max-height'] = '200px';
+    setTimeout(() => {
+        list.style.overflow = 'auto';
+        document.getElementById('addtaskTemplate').setAttribute("onclick", "clickAddTaskTemplate(event)");
+    }, 200);
+}
+
+
+function closeTaskCategoryDropDownList() {
+    if (openTaskCategoryList == false) return;
+    openTaskCategoryList = false;
+    const list = document.getElementById('edit_category_list');
+    document.getElementById('edit_category').style = '';
+    document.getElementById('edit_category').children[1].setAttribute('open', false);
+    list.style.overflow = 'hidden';
+    list.style['max-height'] = '0';
+}
 
 
 function toggleAssignedContactsCheckState(contactsID) {
@@ -10,13 +60,14 @@ function toggleAssignedContactsCheckState(contactsID) {
 
 
 function openAssignedContactsDropDownList() {
+    closeTaskCategoryDropDownList();
     if (openAssignedContactsList == true) return;
     openAssignedContactsList = true;
     document.getElementById('inputAssignContacts').placeholder = 'Search contact';
     showAllAssignedContacts();
     const list = document.getElementById('edit_assigned_list');
     document.getElementById('edit_assigned').style = 'border: 1px solid #29abe2;'
-    document.getElementById('edit_assigned').children[1].style = 'transform: rotate(180deg);'
+    document.getElementById('edit_assigned').children[1].setAttribute('open', true);
     list.style['max-height'] = '200px';
     document.getElementById('inputAssignContacts').focus();
     setTimeout(() => {
@@ -32,12 +83,19 @@ function closeAssignedContactsDropDownList() {
     document.getElementById('addtaskTemplate').removeAttribute("onclick");
     const list = document.getElementById('edit_assigned_list');
     document.getElementById('edit_assigned').style = '';
-    document.getElementById('edit_assigned').children[1].style = '';
+    document.getElementById('edit_assigned').children[1].setAttribute('open', false);
     list.style.overflow = 'hidden';
     list.style['max-height'] = '0';
     const input = document.getElementById('inputAssignContacts');
     input.placeholder = 'Select contacts to assign';
     input.value = '';
+}
+
+
+function closeAllDropDowns() {
+    closeAssignedContactsDropDownList();
+    closeTaskCategoryDropDownList();
+    document.getElementById('addtaskTemplate').removeAttribute("onclick");
 }
 
 
@@ -62,13 +120,15 @@ function changeAssignedContactsSearchTerm() {
 
 
 function clickAddTaskTemplate(event) {
-    const excludedIDs = ['edit_assigned_list', 'edit_assigned', 'inputAssignContacts', 'atncddm']
+    const excludedIDs = ['edit_assigned_list', 'edit_assigned', 'inputAssignContacts', 'atncddm',]
     if (event.target.id.startsWith('assignedContacts_')) return;
     for (let index = 0; index < excludedIDs.length; index++) {
         const elementID = excludedIDs[index];
         if (event.target.id == elementID) return;
     }
-    closeAssignedContactsDropDownList();
+    event.stopPropagation();
+    closeAllDropDowns()
+    // closeAssignedContactsDropDownList();
 }
 
 
@@ -77,4 +137,17 @@ function clickAssignedContactsDropDownList(event) {
     const list = document.getElementById('edit_assigned_list');
     if (list.clientHeight == 0) openAssignedContactsDropDownList();
     else closeAssignedContactsDropDownList();
+}
+
+function clickTaskPriorityDropDownList(event) {
+    event.stopPropagation();
+    const list = document.getElementById('edit_category_list');
+    if (list.clientHeight == 0) openTaskCategoryDropDownList();
+    else closeTaskCategoryDropDownList();
+}
+
+
+function selectTaskCategory(categoryID) {
+    document.getElementById('taskcategory_name').innerHTML = taskCategorys[categoryID].name;
+    closeTaskCategoryDropDownList();
 }
