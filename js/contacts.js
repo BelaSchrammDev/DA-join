d389302d5926439bf8a626ed07493e0c75724b4f
Simon Henke <henke.simon@yahoo.de>
2024-02-09 22:41:44 +0100

    completed contact style

--- js/contacts.js
+++ js/contacts.js
@@ -2,3 +2,10 @@ async function initContactsSite(){
     await initJoin();
     // renderfunctions
 }
+
+
+function contactActive() {
+    let container = document.getElementById('contact1');
+    container.classList.add('contact-data-container-active');
+    container.classList.remove('contact-data-container');
+}
