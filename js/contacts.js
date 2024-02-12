99ffd785c07e4c5e45959b1238af37de99b566b1
Simon Henke <henke.simon@yahoo.de>
2024-02-12 22:00:23 +0100

    added edit window

--- js/contacts.js
+++ js/contacts.js
@@ -11,11 +11,46 @@ function contactActive() {
 }
 
 
-async function showAddToContactWindow() {
+function showAddToContactWindow() {
     let background = document.getElementById('addEditContactContainer');
+    background.innerHTML = `
+        <div onclick="event.stopPropagation()" id="addEditContact" class="add-edit-contact">
+            <div class="add-edit-contact-left">
+                <img src="./img/logo/join-small.png" alt="join-logo">
+                <h2>Add contact</h2>
+                <span>Tasks are better with a team!</span>
+                <div></div>
+            </div>
+            <div class="add-edit-contact-middle">
+                <div>
+                    <img src="./img/icons/contacts/profile.svg" alt="user">
+                </div>
+            </div>
+            <div class="add-edit-contact-right">
+                <div onclick="closeAddToContactWindow()">
+                    <img src="./img/icons/contacts/cancel.svg" alt="exit">
+                </div>
+                <div class="input-container">
+                    <input placeholder="Name" class="input-class name-input" type="text">
+                    <input placeholder="Email" class="input-class email-input" type="email">
+                    <input placeholder="Phone" class="input-class phone-input" type="tel">
+                </div>
+                <div class="button-container">
+                    <button class="cancel-button">
+                        <span>Cancel</span>
+                        <img src="./img/icons/contacts/cancel.svg" alt="cancel">
+                    </button>
+                    <button class="create-button">
+                        <span>Create Contact</span>
+                        <img src="./img/icons/contacts/check-white.svg" alt="check">
+                    </button>
+                </div>
+            </div>
+        </div>
+    `;
     let window = document.getElementById('addEditContact');
     background.classList.remove('hide');
-    await setTimeout(() => {
+    setTimeout(() => {
         background.classList.add('add-edit-contact-container');
     }, 0);
     setTimeout(() => {
@@ -24,16 +59,63 @@ async function showAddToContactWindow() {
 }
 
 
-async function closeAddToContactWindow() {
+function closeAddToContactWindow() {
     let background = document.getElementById('addEditContactContainer');
     let window = document.getElementById('addEditContact');
-    await setTimeout(() => {
+    setTimeout(() => {
         window.style.transform = 'translateX(200%)';
     }, 0);
-    await setTimeout(() => {
+    setTimeout(() => {
         background.classList.remove('add-edit-contact-container');
     }, 500);
     setTimeout(() => {
         background.classList.add('hide');
     }, 700);
 }
+
+
+function showEditWindow() {
+    let background = document.getElementById('addEditContactContainer');
+    background.innerHTML = `
+        <div onclick="event.stopPropagation()" id="addEditContact" class="add-edit-contact">
+            <div class="add-edit-contact-left">
+                <img src="./img/logo/join-small.png" alt="join-logo">
+                <h2>Edit contact</h2> 
+                <div></div>
+            </div>
+            <div class="add-edit-contact-middle">
+                <div>
+                    <img src="./img/icons/contacts/profile.svg" alt="user">
+                </div>
+            </div>
+            <div class="add-edit-contact-right">
+                <div onclick="closeAddToContactWindow()">
+                    <img src="./img/icons/contacts/cancel.svg" alt="exit">
+                </div>
+                <div class="input-container">
+                    <input placeholder="Name" class="input-class name-input" type="text">
+                    <input placeholder="Email" class="input-class email-input" type="email">
+                    <input placeholder="Phone" class="input-class phone-input" type="tel">
+                </div>
+                <div class="button-container">
+                    <button class="cancel-button">
+                        <span>Delete</span>
+                    </button>
+                    <button class="create-button">
+                        <span>Save</span>
+                        <img src="./img/icons/contacts/check-white.svg" alt="check">
+                    </button>
+                </div>
+            </div>
+        </div>
+    `;
+    let window = document.getElementById('addEditContact');
+    background.classList.remove('hide');
+    setTimeout(() => {
+        background.classList.add('add-edit-contact-container');
+    }, 0);
+    setTimeout(() => {
+        window.style.transform = 'translateX(0)';
+    }, 50);
+}
+
