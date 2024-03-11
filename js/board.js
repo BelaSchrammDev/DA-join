c5c6927044f83ee9e1da919f25df3d91339dc511
BelaSchrammDev <belaschramm@aol.de>
2024-03-11 17:21:23 +0100

    resolve #37; split board.js in board.js and board_render.js

--- js/board.js
+++ js/board.js
@@ -1,18 +1,4 @@
 /**
- * An array of objects representing each row in the board.
- * Each object contains the id, name, and addbutton properties.
- * 
- * @type {Array<{id: string, name: string, addbutton: boolean}>}
- */
-const rowIdName = [
-    { id: 'todo', name: 'ToDo', addbutton: true },
-    { id: 'inprogress', name: 'In Progress', addbutton: true },
-    { id: 'awaitfeedback', name: 'Await Feedback', addbutton: true },
-    { id: 'done', name: 'Done', addbutton: false },
-]
-
-
-/**
  * The ID of the currently dragged task.
  * @type {string}
  */
@@ -34,19 +20,7 @@ let current_taskID = '';
 
 
 /**
- * A test function to display a request window.
- */
-function testrequest() {
-    let div = getElement('requestdiv');
-    div.innerHTML = getRequestWindowHTML(1, 'testfrage?');
-    showOverlay('requestdiv');
-}
-
-
-/**
  * Initializes the board site.
- * 
- * @returns {Promise<void>}
  */
 async function initBoardSite() {
     await initJoin();
@@ -116,91 +90,6 @@ function closeOverlay(event = null) {
 
 
 /**
- * Renders the rows of the board.
- */
-function renderRows() {
-    let rowsHTML = '';
-    for (let index = 0; index < rowIdName.length; index++) {
-        rowsHTML += getTaskRowHTML(rowIdName[index]);
-    }
-    setInnerHTML('board_task_rows', rowsHTML);
-}
-
-
-/**
- * Renders all tasks in the related rows.
- * 
- * @param {string} [filter=''] - The filter string to apply to the tasks.
- */
-function renderTasks(filter = '') {
-    resetAllDropDowns();
-    clearRows();
-    for (let index = 0; index < sessionTasks.length; index++) {
-        const task = sessionTasks[index];
-        if (filter == ''
-            || task.title.toLowerCase().includes(filter.toLowerCase())
-            || task.description.toLowerCase().includes(filter.toLowerCase())) {
-
-            const tasksHTML = `<div id="taskcard_${task.id}">${getTaskHTML(task)}</div>`;
-            const tasklist = document.getElementById('tasklist_' + task.status);
-            if (tasklist) tasklist.innerHTML += tasksHTML;
-            addDropDownList('taskmove_' + task.id, openMoveMenu, 'taskmove_' + task.id);
-        }
-    }
-    renderEmptyRowMessage();
-}
-
-
-/**
- * Renders an info message in empty rows.
- */
-function renderEmptyRowMessage() {
-    for (let index = 0; index < rowIdName.length; index++) {
-        const tasklist = document.getElementById('tasklist_' + rowIdName[index].id);
-        if (tasklist.innerHTML == '') {
-            tasklist.innerHTML = `<div class="board_task_empty">No tasks<br>${rowIdName[index].name}</div>`;
-        }
-    }
-}
-
-
-/**
- * Clears all the rows of the board.
- */
-function clearRows() {
-    for (let index = 0; index < rowIdName.length; index++) {
-        const tasklist = document.getElementById('tasklist_' + rowIdName[index].id);
-        tasklist.innerHTML = ``;
-    }
-}
-
-
-/**
- * Renders and shows the big task view window.
- * 
- * @param {string} taskID - The ID of the task to display.
- */
-function showBigTaskView(taskID) {
-    const task = sessionTasks.find(t => t.id == taskID);
-    if (task) {
-        current_taskID = taskID;
-        updateBigTaskView(task);
-        showOverlay('task_big')
-    }
-}
-
-
-/**
- * Updates the content of the big task view window.
- * 
- * @param {Object} task - The task object to display.
- */
-function updateBigTaskView(task) {
-    setInnerHTML('task_big_showing', getBigTaskHTML(task));
-}
-
-
-/**
  * Shows the add task overlay window.
  * 
  * @param {string} newtaskStatus - The status of the new task.
@@ -209,20 +98,6 @@ function showAddTaskOverlay(newtaskStatus) {
     presetStatusByAddTask = newtaskStatus;
     getElement('addtask_form').reset();
     showOverlay('addtask_overlay');
-}
-
-
-/**
- * Updates the mini task card.
- * 
- * @param {string} taskID - The ID of the task to update.
- */
-function updateMiniTaskCard(taskID) {
-    const task = sessionTasks.find(t => t.id == taskID);
-    const taskCard = getElement('taskcard_' + taskID);
-    if (task && taskCard) {
-        taskCard.innerHTML = getTaskHTML(task);
-    }
 }
 
 
